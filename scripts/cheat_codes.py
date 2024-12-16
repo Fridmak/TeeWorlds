e17777f974c468467757bb019f8c17da4dce3b18
Fridmak <apbatin2005@gmail.com>
2024-12-17 02:18:12 +0500

    физика рикошета работает отлично.

--- scripts/cheat_codes.py
+++ scripts/cheat_codes.py
@@ -1,7 +1,7 @@
 cheat_cods = {
-    "52Aflh": "full_hp",
+    "SILFY": "full_hp",
     "fGriOdmaDk": "immortality",
-    "bFoInRaEn": "damage_up",
+    "POWER": "damage_up",
     "BiGDiCk" : "no_recoil",
     "Jumper" : "no_jump_limits",
     "BiGBrAiN" : "no_knockback",
